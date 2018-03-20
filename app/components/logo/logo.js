23c16c8001900f221afb66b89f0ac34fd83c1614
Mehdi33 <mehdi.slayki@epsi.fr>
2018-03-20 09:58:23 +0100

    ajout textInput

--- app/components/logo/logo.js
+++ app/components/logo/logo.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+
+import { View, ImageBackground, Image, Text } from 'react-native';
 
 import styles from './styles';
 
 const Logo = () => (
   <View style={styles.container}>
-    <Image
+    <ImageBackground
       resizeMode="contain"
       style={styles.containerImage}
       source={require('./images/background.png')}
     >
       <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
-    </Image>
+    </ImageBackground>
     <Text style={styles.text}>FFRSIM</Text>
   </View>
 );
--- app/index.js
+++ app/index.js
@@ -5,6 +5,9 @@ import Home from './screens/home';
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
   $white: '#fff',
+  $lightGray: '#F0F0F0',
+  $border: '#979797',
+  $inputText: '#797979',
 });
 
 export default () => <Home />;
