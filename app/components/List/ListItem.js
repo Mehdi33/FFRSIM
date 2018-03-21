5bf79a069705aec765bd3e122cafaa5387b173e9
Mehdi33 <mehdi.slayki@epsi.fr>
2018-03-21 14:09:55 +0100

    List Item

---
+++ app/components/List/ListItem.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { View, Text, TouchableHightlight } from 'react-native';
+import Icon from './Icon';
+
+import styles from './styles';
+
+const ListItem = ({ text, selected = false, onPress, checkmark = true, visible = true }) => (
+  <TouchableHightlight onPress={onPress} underlayColor={styles.$underlayColor}>
+    <View style={styles.row}>
+      <Text style={styles.text}>{text}</Text>
+      {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+    </View>
+  </TouchableHightlight>
+);
+
+ListItem.propTypes = {
+  text: PropTypes.string,
+  onPress: PropTypes.func,
+  selected: PropTypes.bool,
+  checkmark: PropTypes.bool,
+  visible: PropTypes.bool,
+};
+
+export default ListItem;
